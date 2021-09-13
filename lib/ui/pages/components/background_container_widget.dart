122675e31cbd00b371cbc999ed3dad37a8ac18b2
caio <caioj3505@gmail.com>
2021-09-13 09:11:55 -0300

    create BackgroundContainer widget

---
+++ lib/ui/pages/components/background_container_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class BackgroundContainerWidget extends StatelessWidget {
+  const BackgroundContainerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          child: Column(
+            children: [
+              const SizedBox(height: 100),
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: NeverScrollableScrollPhysics(),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).backgroundColor,
+                      borderRadius:
+                          const BorderRadius.only(topLeft: Radius.circular(60)),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 35),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Todo',
+                            style: Theme.of(context).textTheme.headline1,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Divider(
+                          color: Colors.black.withOpacity(0.3),
+                          height: 2,
+                          thickness: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
