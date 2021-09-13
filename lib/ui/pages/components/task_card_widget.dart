90fba0d1b15f718ea8d8839a03b4975977dff32d
caio <caioj3505@gmail.com>
2021-09-13 09:19:56 -0300

    create TaskCard widget

---
+++ lib/ui/pages/components/task_card_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:new_taskee/ui/pages/task/task_viewmodel.dart';
+
+class TaskCard extends StatelessWidget {
+  final TaskViewModel task;
+  final Color cardColor;
+  final Widget icon;
+  final Widget iconBack;
+
+  const TaskCard({
+    Key? key,
+    required this.task,
+    required this.cardColor,
+    required this.icon,
+    required this.iconBack,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Card(
+        elevation: 10,
+        color: cardColor,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(left: 10, right: 5, bottom: 30, top: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              iconBack,
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      task.title,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4!
+                          .copyWith(color: Colors.white),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      task.subtitle,
+                      style: Theme.of(context).textTheme.bodyText2,
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              icon,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
