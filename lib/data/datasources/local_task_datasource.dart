33dfeadddfcdeb8355460fd8fabf59c70e8927be
caio <caioj3505@gmail.com>
2021-09-14 16:03:47 -0300

    remove entity manager

--- lib/data/datasources/local_task_datasource.dart
+++ lib/data/datasources/local_task_datasource.dart
@@ -2,13 +2,12 @@ import 'dart:convert';
 
 import 'package:new_taskee/data/cache/cache_storage.dart';
 import 'package:new_taskee/data/helpers/exceptions/cache_exception.dart';
-import 'package:new_taskee/data/helpers/mixins/entity_manager.dart';
 import 'package:new_taskee/data/models/models.dart';
 import 'package:new_taskee/domain/datasources/datasources.dart';
 import 'package:new_taskee/domain/entities/task_entity.dart';
 import 'package:new_taskee/domain/helpers/parameters/task_parameters.dart';
 
-class LocalTaskDatasource with EntityManager implements ITaskDatasource {
+class LocalTaskDatasource implements ITaskDatasource {
   final CacheStorage storage;
   LocalTaskDatasource(this.storage) {
     _init();
@@ -24,7 +23,7 @@ class LocalTaskDatasource with EntityManager implements ITaskDatasource {
   List<TaskEntity> read() {
     final data = storage.read('tasks');
     if (data != null) {
-      return mapToEntity(data);
+      return _mapToEntity(data);
     } else if (data == null) {
       return [];
     } else {
@@ -62,4 +61,7 @@ class LocalTaskDatasource with EntityManager implements ITaskDatasource {
     final data = storage.read('tasks');
     if (data == null) storage.save(key: 'tasks', value: '{}');
   }
+
+  List<TaskEntity> _mapToEntity(dynamic list) =>
+      list.map((entity) => TaskModel.fromEntity(entity).toJson()).toList();
 }
