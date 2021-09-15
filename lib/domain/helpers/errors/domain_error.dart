ef95d612bda98c53eff309950355c954600e325a
caio <caioj3505@gmail.com>
2021-09-15 07:51:01 -0300

    add DomainErrorExtensions message method

--- lib/domain/helpers/errors/domain_error.dart
+++ lib/domain/helpers/errors/domain_error.dart
@@ -2,3 +2,14 @@ enum DomainError {
   somethingWrong,
   cacheFailure,
 }
+
+extension DomainErrorExtensions on DomainError {
+  String get message {
+    switch (this) {
+      case DomainError.somethingWrong:
+        return 'Something wrong, try again!';
+      case DomainError.cacheFailure:
+        return 'Something wrong with the memory, try again!';
+    }
+  }
+}
