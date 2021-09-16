b79b6407e1f68678f56d28d844f3c6ad02aa3adf
caio <caioj3505@gmail.com>
2021-09-16 10:04:14 -0300

    add enabled in device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:new_taskee/main/factories/pages/task/task_page_factory.dart';
 import 'package:new_taskee/ui/components/app_theme.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (_) => MyApp()));
+  runApp(DevicePreview(
+    builder: (_) => MyApp(),
+    enabled: !kReleaseMode,
+  ));
 }
 
 class MyApp extends StatelessWidget {
